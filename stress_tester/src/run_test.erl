4195c60f894193b2bf23e6fb351058071a333ace
Dane Artis <daneartis4@gmail.com>
2023-03-28 11:11:58 -0600

    Get rid of extra test function file

--- stress_tester/src/run_test.erl
+++
@@ -1,9 +0,0 @@
--module(run_test).
-
--export([single_package/0, ten_package/0]).
-
-single_package() ->
-  stresser:start_all("server.dartis.dev", tester, 1).
-
-ten_package() ->
-  stresser:start_all("server.dartis.dev", tester, 10).
